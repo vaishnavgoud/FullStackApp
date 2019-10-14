03538e70110d98e10d14eb9ab8414e1f63d7014a
vgangarapu <vgangarapu@am.sony.com>
2019-10-14 21:17:09 +0530

    Build files

---
+++ client/build/precache-manifest.591a597b9e73d123b1ac416bcabfd3a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0519709e65fcadba7ee57ee21b842a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30546ef763a5a3c72cb6",
+    "url": "/static/js/2.54a4bf86.chunk.js"
+  },
+  {
+    "revision": "4efdd3caed97b78014d9",
+    "url": "/static/js/main.08c617c9.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  }
+]);
